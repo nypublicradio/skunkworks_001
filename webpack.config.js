e6959fb83e5fb75e90cea6fc5f9982be1ee8f34a
Michael Hearn <git@mikehearn.net>
2018-09-19 15:24:09 -0400

    Add IE11 babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ['@babel/polyfill', './src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
