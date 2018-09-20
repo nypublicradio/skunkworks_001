f384bb6ca8c7bfc77e60f6b31775690baf3d9eb9
Brian Whitton <me@brianwhitton.com>
2018-09-20 13:26:24 -0400

    templetize asset paths in index file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 /* eslint-env node */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+
+const ASSET_PATH = process.env.AWS_S3_KEY || '/';
 
 module.exports = {
   entry: ['@babel/polyfill', './src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.js',
+    publicPath: ASSET_PATH,
   },
   module: {
     rules: [
@@ -27,7 +31,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/static/index.html'),
-      AWS_S3_KEY: process.env.AWS_S3_KEY || '',
+      hash: true,
+      inject: false,
+    }),
+    new HtmlWebpackIncludeAssetsPlugin({
+      assets: ['css/style.css', 'data/neighb_coords.js'],
+      files: 'index.html',
+      append: false,
+      hash: true,
     }),
   ],
 };
