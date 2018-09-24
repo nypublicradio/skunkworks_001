96499430efd7f8bd92611c0a4c5a1331f182810e
sylviawhoa <sylviaples@gmail.com>
2018-09-24 17:02:39 -0400

    generates unique index files for each district

--- data_build/gen_index_files.py
+++ data_build/gen_index_files.py
@@ -1,19 +1,46 @@
 import os
 
 
+"""
+To make index.html
+
+Have server running with env variables
+
+On demo:
+AWS_S3_KEY=voter-turnout
+BASE_URL=https://demo.project.gothamist.com
+IS_SCREENSHOTTING=false
+
+
+On Prod:
+
+AWS_S3_KEY=voter-turnout
+BASE_URL=https://project.gothamist.com
+IS_SCREENSHOTTING=false
+
+
+Npm run build
+
+Get index file from dist
+"""
+
+
+output_dir = './district_folders_for_s3/'
+
 # get text of index.html to write to file in every folder
-with open('../src/static/index.html', 'r') as f:
-    index_text = f.readlines()
+with open('../dist/index.html', 'r') as f:
+    index_text = f.read()
 
 # get districts from text file
 with open('../src/static/data/election_districts.txt', 'r') as f:
-    districts = f.readlines()
-    districts = [line.strip('\n') for line in districts]
+    districts = f.read()
+    districts = districts.split("\n")[:-1]
 
 # for every district, create a folder (if not exists) and write index.html to folder
 for district in districts:
-    directory = '../src/static/{}'.format(district)
+    directory = output_dir + district
     if not os.path.exists(directory):
         os.makedirs(directory)
     with open(directory + '/index.html', 'w') as f:
-        f.writelines(index_text)
+        directory_index_text = index_text.replace("{{id}}", district)
+        f.write(directory_index_text)
