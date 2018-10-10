7834e86e24ab4a30de261518315f5cf0d5ae5488
Brian Whitton <me@brianwhitton.com>
2018-10-10 13:35:04 -0400

    forms will be in html

--- src/js/routes.js
+++ src/js/routes.js
@@ -24,15 +24,6 @@ const IndexRoute = {
         name: 'address',
         selector: '#address-form__address-input',
         message: 'Please enter an address.',
-      // }, {
-      //   name: 'email',
-      //   selector: '#address-form__email-input',
-      //   message: 'Please enter an email.',
-      // }, {
-      //   name: 'legal',
-      //   selector: '#legal',
-      //   message: 'Please agree to the terms',
-      //   validation: el => el.checked,
       }]
     });
 
@@ -109,20 +100,6 @@ const DistrictRoute = {
           name: 'address',
           selector: '#address-form__address-input',
           message: 'Please enter an address.',
-        }]
-      });
-      bindAddressFormEvents({
-        form: '#email-form',
-        errors: '#email-errors',
-        fields: [{
-          name: 'email',
-          selector: '#address-form__email-input',
-          message: 'Please enter an email.',
-        }, {
-          name: 'legal',
-          selector: '#legal',
-          message: 'Please agree to the terms',
-          validation: el => el.checked,
         }]
       });
     }
